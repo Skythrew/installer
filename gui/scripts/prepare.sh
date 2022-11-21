9ae4cba4faa2f8d7cac3b76c44a78d9d7b3eda9e
Bastien <77548264+Babilinx@users.noreply.github.com>
2022-11-21 18:57:27 +0100

    Update prepare.sh
    From install.sh

--- gui/scripts/prepare.sh
+++ gui/scripts/prepare.sh
@@ -6,16 +6,35 @@
 
 echo 'Preparation started.'
 
-# Pretending to do something
-echo 'Pretending to do something'
+# Install squirrel
+echo "Installing squirrel on the host system..."
+git clone --branch 1.0.2 https://github.com/stock-linux/squirrel.git
+ln -s $PWD/squirrel/squirrel /bin/squirrel
+echo "Done"
 
-for i in {1..5}
-do
-    sleep 1
-    echo -n '.'
-done
+echo "Configuring squirrel..."
+echo -e "#!/bin/sh\npython3 $PWD/squirrel/main.py \$@" > squirrel/squirrel
+chmod +x squirrel/squirrel
+mkdir -p $PWD/squirrel/dev/etc/squirrel/ $PWD/squirrel/dev/var/squirrel/repos/dist/ $PWD/squirrel/dev/var/squirrel/repos/local/ $PWD/squirrel/dev/var/squirrel/repos/local/main/
+echo "Done"
 
-echo
-echo 'Preparation completed.'
+echo "Installing squirrel dependencies..."
+pip3 install docopt pyaml requests packaging
+echo "Done"
+
+echo "Final configuration for squirrel..."
+echo "configPath = '$PWD/squirrel/dev/etc/squirrel/'" > squirrel/utils/config.py
+echo "distPath = '$PWD/squirrel/dev/var/squirrel/repos/dist/'" >> squirrel/utils/config.py
+echo "localPath = '$PWD/squirrel/dev/var/squirrel/repos/local/'" >> squirrel/utils/config.py
+
+echo "main http://stocklinux.hopto.org:8080/45w22/main" > squirrel/dev/etc/squirrel/branches
+echo "cli http://stocklinux.hopto.org:8080/45w22/cli" >> squirrel/dev/etc/squirrel/branches
+echo "gui http://stocklinux.hopto.org:8080/45w22/gui" >> squirrel/dev/etc/squirrel/branches
+echo "extra http://stocklinux.hopto.org:8080/45w22/extra" >> squirrel/dev/etc/squirrel/branches
+
+touch $PWD/squirrel/dev/var/squirrel/repos/local/main/INDEX
+echo "Done"
+
+echo "Everything is configured !"
 
 exit 0
